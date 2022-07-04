843ee1fcbf3e1268c769febcf16d1b8d299a5be8
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-07-05 00:08:13 +0300

    implemented geting and adding contacts to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,23 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const localContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (localContacts) {
+      this.setState({ contacts: localContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contact !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   setContact = contact => {
     const isContactDuplicate = this.state.contacts.find(
